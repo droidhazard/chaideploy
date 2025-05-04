172ac0b6dc3cb6cf770ac5d5f5c9b03d4e512895
Zohaib Ali <www.zohaib.0335@gmail.com>
2025-05-04 13:10:03 +0500

    '/jokes' route

--- 2. connecting front-end and backend/backend/server.js
+++ 2. connecting front-end and backend/backend/server.js
@@ -9,6 +9,33 @@ app.get("/", (req, res) => {
   res.send("server is ready");
 });
 
+// * Get a list of 5 jokes
+app.get("/jokes", (req, res) => {
+  const jokes = [
+    {
+      id: 1,
+      title: "Joke 1",
+      content: "This is joke 1",
+    },
+    {
+      id: 2,
+      title: "Joke 2",
+      content: "This is joke 2",
+    },
+    {
+      id: 3,
+      title: "Joke 3",
+      content: "This is joke 3",
+    },
+    {
+      id: 4,
+      title: "Joke 4",
+      content: "This is joke 4",
+    },
+  ];
+  res.send(jokes);
+});
+
 app.listen(PORT, () => {
   console.log(`app is listening:${PORT}`);
 });
