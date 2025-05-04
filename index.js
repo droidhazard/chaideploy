6fa92f8cd9995665d1496192576deee81bb20258
Zohaib Ali <www.zohaib.0335@gmail.com>
2025-05-04 06:42:13 +0500

    dotenv

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express')
 const app = express()
-const port = 3000
+require('dotenv').config()
+
+const port = process.env.PORT
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) => {
+    res.send('hello')
+})
+
+app.get('/login', (req, res)=> {
+  res.send('<h1>Please login at chai aur code</h1>')
+})
+
+app.get('/youtube', (req, res) => {
+  res.send('<h2>Chai aur Code</h2>')
 })
 
 app.listen(port, () => {
